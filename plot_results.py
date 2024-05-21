6865ea89b4f42678ff0c3718e52f2d0809843ee4
npalex <nalexander460@gmail.com>
2024-05-21 10:45:00 -0700

    added streamlines animation

--- plot_results.py
+++ plot_results.py
@@ -77,6 +77,9 @@ xgrid, ygrid = np.meshgrid(x, y)
 fig = plt.figure(figsize=(6,6))
 ax = plt.axes(xlim=(-0.02, 1.02), ylim=(-0.02, 1.1))            # creates axes at specifed limits, (gca() not required)
 
+#----------------------------------------------
+#-- plot vector field and pressure distribution
+#----------------------------------------------
 #-- define function, which is an argument for the method animation.FuncAnimation() and is called for each frame
 def fplot(frame_number):
     
@@ -102,5 +105,36 @@ plt.close()                        #-- removes residual plot at final time
 #HTML(anim.to_jshtml())             #-- print animation in jupyter notebook
 
 #-- save animation as an html file
-with open("Results.html", "w") as f:
+with open("Results_vector_field.html", "w") as f:
+    print(anim.to_html5_video(embed_limit=None), file=f)
+    
+#-------------------
+#-- plot streamlines
+#-------------------
+fig = plt.figure(figsize=(6,6))
+ax = plt.axes(xlim=(-0.02, 1.02), ylim=(-0.02, 1.1))            # creates axes at specifed limits, (gca() not required)
+    
+#-- define function, which is an argument for the method animation.FuncAnimation() and is called for each frame
+def fplot(frame_number):
+    
+    ax.clear()
+    
+    #-- plot velocity distribution
+    ax.streamplot(xgrid, ygrid, q[0,frame_number,::q_step,::q_step], q[1,frame_number,::q_step,::q_step], color = "black", linewidth = .5, density=.8, broken_streamlines=False)  
+
+    #-- set axex limits and tick marks
+    ax.set_xticks(np.arange(0, 1.2, .2))
+    ax.set_yticks(np.arange(0, 1.2, .2))
+    ax.set_xlim(left = 0, right = 1.)
+    ax.set_ylim(bottom = 0, top = 1.)
+    
+    return()
+
+#-- generate animation
+anim = animation.FuncAnimation(fig = fig, func = fplot, frames=int(steps), interval=80, repeat=False)
+plt.close()                        #-- removes residual plot at final time
+#HTML(anim.to_jshtml())             #-- print animation in jupyter notebook
+
+#-- save animation as an html file
+with open("Results_streamlines.html", "w") as f:
     print(anim.to_html5_video(embed_limit=None), file=f)
